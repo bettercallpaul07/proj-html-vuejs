a302c41001b22f68d0bd59e03b4be4b5ad7dd1ac
Paolo Rapuano <bettercallpaul07@gmail.com>
2023-02-15 18:29:29 +0100

    Shop+Testimonial Added

--- src/components/AppShop.vue
+++ src/components/AppShop.vue
@@ -18,6 +18,45 @@ export default {
                 <img src="../assets/img/svg/arrowleft.svg">
                 <img class="green" src="../assets/img/svg/arrowright.svg">
             </div>
+            <div class="shop">
+                <div class="item">
+                    <div class="imgbox">
+                        <img src="../assets/img/png/s1.png">
+                    </div>
+                    <div>
+                        <h4>Sony PlayStation 5</h4>
+                    </div>
+
+                </div>
+                <div class="item">
+                    <div class="imgbox">
+                        <img src="../assets/img/png/s2.png">
+                    </div>
+                    <div>
+                        <h4>Asus Rog</h4>
+                    </div>
+
+                </div>
+                <div class="item">
+                    <div class="imgbox">
+                        <img src="../assets/img/png/s3.png">
+                    </div>
+                    <div>
+                        <h4>Vr Visore Facebook</h4>
+                    </div>
+
+                </div>
+                <div class="item">
+                    <div class="imgbox">
+                        <img src="../assets/img/png/s4.png">
+                    </div>
+                    <div>
+                        <h4>Beats Studio 3 Cuffie</h4>
+                    </div>
+
+                </div>
+
+            </div>
 
         </div>
 
@@ -35,13 +74,47 @@ section {
     background-image: url(../assets/img/png/shop-bg.png);
     background-size: cover;
     background-repeat: no-repeat;
-    filter: opacity(60%);
-    z-index: 3;
-    height: 400px;
+    height: 500px;
 
-    .arrowList {
-        z-index: 2;
+    .shop {
+        display: flex;
+        align-items: center;
+
+        .item {
+
+            .imgbox {
+
+                width: 175px;
+                height: 150px;
+                margin-right: 1rem;
+                border-radius: 0.5rem;
+                background-color: $darkblue-bg;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+
+                img {
+                    max-width: 150px;
+                    max-height: 150px;
+                    padding: 1rem;
+                }
+
+
+
+            }
+
+            h4 {
+                padding-top: 1rem;
+                color: white;
+            }
+
+        }
+
+
+
 
     }
+
+
 }
 </style>
--- src/components/AppTestimonial.vue
+++ src/components/AppTestimonial.vue
@@ -9,7 +9,31 @@ export default {
 <template>
 
     <section>
-        
+
+        <div class="mediumContainer">
+            <div class="title-section">
+                <h5>Testimonial</h5>
+                <h2>What Our Client Say</h2>
+            </div>
+
+            <div class="arrowList">
+                <img src="../assets/img/svg/arrowleft.svg">
+                <img class="green" src="../assets/img/svg/arrowright.svg">
+            </div>
+
+            <div class="clientImage">
+                <img src="../assets/img/png/test1.png">
+                <img src="../assets/img/png/test2.png">
+                <img src="../assets/img/png/test3.png">
+                <img src="../assets/img/png/test4.png">
+
+
+            </div>
+
+
+        </div>
+
+
     </section>
 
 
@@ -18,5 +42,20 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+section {
 
+    background-image: url(../assets/img/png/test-grouptwo.png);
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 500px;
+
+    .clientImage {
+        display: flex;
+
+        img {
+            max-width: 200px;
+            margin-right: 1rem;
+        }
+    }
+}
 </style>
